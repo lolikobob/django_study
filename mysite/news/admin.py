77b78173ce061d31544680acc1a6e5c607d9dd96
lolikobob <lolikobob@gmail.com>
2022-10-23 02:22:19 +0600

    Урок 40. Django Debug Toolbar Урок 41. Кастомизация админки

--- mysite/news/admin.py
+++ mysite/news/admin.py
@@ -1,13 +1,26 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import News, Category
 
 
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'category', 'created_at', 'updated_at', 'is_published')
+    list_display = ('id', 'title', 'category', 'created_at', 'updated_at', 'is_published', 'get_photo')
     list_display_links = ('title', )
     search_fields = ('title', 'content')
-    list_editable = ('is_published', )
+    list_editable = ('is_published',)
     list_filter = ('is_published', 'category')
+    fields = ('title', 'category', 'content', 'photo', 'get_photo', 'is_published', 'views', 'created_at', 'updated_at')
+    readonly_fields = ['created_at', 'updated_at', 'views', 'get_photo']
+    save_on_top = True
+
+    def get_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}" width="75">')
+        else:
+            return "-"
+
+    get_photo.short_description = 'Миниатюра'
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -18,3 +31,6 @@ class CategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(News, NewsAdmin)
 admin.site.register(Category, CategoryAdmin)
+
+admin.site.site_title = 'Управление новостями'
+admin.site.site_header = 'Управление новостями'
