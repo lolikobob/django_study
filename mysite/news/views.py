ea4be86759e6c55f6b8aa9075e2d79e16c23db2c
lolikobob <lolikobob@gmail.com>
2022-10-24 01:49:03 +0600

    Урок 42.Миксины Урок 43.Постраничная навигация. Часть 1 - Функция Урок 44.Постраничная навигация. Часть 2 - Классы

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render, get_object_or_404, redirect
+# from django.urls import reverse_lazy
+# from .utils import MyMixin
+from django.shortcuts import render  # get_object_or_404, redirect
 from django.views.generic import ListView, DetailView, CreateView
-from django.urls import reverse_lazy
 from .models import News, Category
 from .forms import NewsForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.paginator import Paginator
 
 
 class HomeNews(ListView):
@@ -10,6 +13,7 @@ class HomeNews(ListView):
     template_name = 'news/home_news_list.html'
     context_object_name = 'news'
     # extra_context = {'title': 'Главная'}
+    paginate_by = 2
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -25,6 +29,7 @@ class NewsByCategory(ListView):
     template_name = 'news/home_news_list.html'
     context_object_name = 'news'
     allow_empty = False
+    paginate_by = 2
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -43,10 +48,20 @@ class ViewNews(DetailView):
     # pk_url_kwarg = 'news_id'
 
 
-class CreateNews(CreateView):
+class CreateNews(LoginRequiredMixin, CreateView):
     form_class = NewsForm
     template_name = 'news/add_news.html'
     # success_url = reverse_lazy('home')
+    # login_url = '/admin/'
+    raise_exception = True
+
+
+def test(request):
+    objects = ['john1', 'paul2', 'george3', 'ringo4', 'john5', 'paul6', 'george7']
+    paginator = Paginator(objects, 2)
+    page_num = request.GET.get('page', 1)
+    page_objects = paginator.get_page(page_num)
+    return render(request, 'news/test.html', {'page_obj': page_objects})
 
 
 # def index(request):
